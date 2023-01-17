9be03c1a1c41ffe7e7debd17e882bdbf18f6f482
Dmytro Hanych <Hanych_IT@ukr.net>
2023-01-17 23:10:04 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import { GlobalStyle } from 'styleConfig/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styleConfig/theme';
 import 'react-toastify/dist/ReactToastify.css';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
